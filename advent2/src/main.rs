980bb28098dc300c1e95925e9e54ea7d9ba3a13a
Alessandro Dalfovo <alessandro.dalfovo@paros.srl>
2017-12-15 17:19:20 +0100

    Second step of the second day

--- advent2/src/main.rs
+++ advent2/src/main.rs
@@ -6,30 +6,34 @@ fn main() {
     println!("{}", parse_file(String::from("input")))
 }
 
-
 fn parse_file(filename: String) -> u32 {
     let f = File::open(filename).unwrap();
     let file = BufReader::new(&f);
-    
+
     file.lines().fold(0, |mut acc, line| {
-        acc += parse(line.unwrap());
-        return acc
+      acc += dividable(line.unwrap());
+      return acc
     })
-
 }
 
-fn parse(numbers: String) -> u32 {
-    let parts:Vec<u32> = numbers
-        .split_whitespace()
-        .map(|s| s.parse::<u32>().unwrap()).collect();
-    
-    let maxvalue = parts.iter().max().unwrap();
-    let minvalue = parts.iter().min().unwrap();
 
-    println!("{} > {}", maxvalue, minvalue);
-    maxvalue - minvalue
+fn dividable(numbers: String) -> u32 {
+    let mut parts:Vec<u32> = numbers
+    .split_whitespace()
+    .map(|s| s.parse::<u32>().unwrap()).collect();
+
+    let (first, second) = asd(parts.pop().unwrap(), parts);
+
+    return first / second
+}   
+
+fn asd(first: u32, mut lst:Vec<u32>) -> (u32, u32) {
+    for second in lst.iter() {
+        if (first % second) == 0 { return (first, *second) }
+        if (second % first) == 0 { return (*second, first) }
+    }
+    return asd(lst.pop().unwrap() , lst)
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -37,12 +41,17 @@ mod tests {
     
     #[test]
     fn given_a_string_with_two_number_returns_the_sum_of_them() {
-        assert_eq!(67, parse(String::from("15   52")))
+        assert_eq!(2, dividable(String::from("5   10")))
     }
 
     #[test]
+    fn given_a_string_with_two_number_returns_thess_sum_of_them() {
+        assert_eq!(2, dividable(String::from("31   5   10")))
+    }
+
+    #[test] 
     fn it_sums_only_the_max_and_the_min_on_the_row() {
-        assert_eq!(53, parse(String::from("15 1 52")))
+        assert_eq!(5, dividable(String::from("15 7 3")))
     }
 
 }
